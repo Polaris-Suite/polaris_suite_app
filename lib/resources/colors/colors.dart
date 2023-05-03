8d57197442e28eb5b5c58f3aff2ba8632b602565
Prasiddha Khadka <be2018se630@gces.edu.np>
2023-05-03 09:16:02 +0545

    setting up

---
+++ lib/resources/colors/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color.fromRGBO(83, 91, 242, 1);
+  static const Color secondaryColor = Color.fromRGBO(255, 171, 18, 1);
+  static const Color neutralDark = Color.fromRGBO(19, 21, 21, 1);
+  static const Color neutralGray = Color.fromRGBO(19, 21, 21, .3);
+  static const Color danger = Color.fromRGBO(218, 73, 68, 1);
+  static const Color success = Color.fromRGBO(79, 119, 45, 1);
+}
---
+++ lib/resources/urls/app_urls.dart
@@ -0,0 +1,7 @@
+class AppUrls {
+  static var baseUrl = "";
+
+  static var loginEndPoints = "$baseUrl + ";
+
+  static var registerEndPoints = "";
+}
